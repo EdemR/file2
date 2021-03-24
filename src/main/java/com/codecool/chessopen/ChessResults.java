615851f7d5c812e4b7cd1b79e8d091d6efb313d8
EdemR <teboldadam@gmail.com>
2021-03-24 16:48:47 +0100

    Clean code

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,14 +1,11 @@
 package com.codecool.chessopen;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ChessResults {
 
